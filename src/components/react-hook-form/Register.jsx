c8cbbe9e1919dd098d47ad60800e2ba9809d6e45
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-10 17:06:38 +0530

    validation using react hook form.

--- src/components/react-hook-form/Register.jsx
+++ src/components/react-hook-form/Register.jsx
@@ -1,17 +1,14 @@
 import { useForm } from "react-hook-form";
 
-export default function Register() {
+export default function App() {
   const { register, handleSubmit } = useForm();
   const onSubmit = data => console.log(data);
    
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} />
-      <select {...register("gender")}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
+      <input {...register("firstName", { required: true, maxLength: 20 })} />
+      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+      <input type="number" {...register("age", { min: 18, max: 99 })} />
       <input type="submit" />
     </form>
   );
